fa7862019d5c643f1fbeb05599b512e52bf4edbd
Piyush Puniya <75742731+piyush-hack@users.noreply.github.com>
2022-05-05 00:09:28 +0530

    new commit

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-const Path = require('path');
-const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+
+const app = express();
+
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 app.use(express.static(__dirname + '/public'));
 app.use('/uploads', express.static('uploads'));
+app.use('/api/scrape', require('./routes/scrape'));
 
 app.get('/', (req, res) => {
     res.send('Welcome');
 })
 
-app.use('/api/scrape', require('./routes/scrape'));
 
 
 app.listen(port, () => {
