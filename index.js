942aab42ffd199bfa1426ba9198bf0930c6cebab
Piyush Puniya <75742731+piyush-hack@users.noreply.github.com>
2022-05-04 23:58:39 +0530

    new commit

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ app.use(cors())
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/uploads', express.static('uploads'));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Welcome')
