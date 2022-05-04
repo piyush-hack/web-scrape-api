64bd5818b375ac815dc20bc43c8bc8006bd2a5c9
Piyush Puniya <75742731+piyush-hack@users.noreply.github.com>
2022-05-04 23:47:36 +0530

    new commit

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var cors = require('cors');
 const Path = require('path');
 const app = express()
 const port = process.env.PORT || 5000;
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200
+}))
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/uploads', express.static('uploads'));
