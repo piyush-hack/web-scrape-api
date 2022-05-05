2900a920510610bb5b0e0efd7784eeed1580d6ce
Piyush Puniya <75742731+piyush-hack@users.noreply.github.com>
2022-05-05 12:36:18 +0530

    new ch

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express();
 app.use(cors());
 
 const bodyParser = require('body-parser');
-const request = require('request-promise');
-const cheerio = require('cheerio');
-var himalaya = require('himalaya');
-const sanitizeHtml = require('sanitize-html');
 
 const port = process.env.PORT || 5000;
 
@@ -23,47 +19,6 @@ app.get('/', (req, res) => {
     res.send('Welcome');
 })
 
-
-
-app.post('/site', async (req, res) => {
-    console.log("body ", req.body);
-    await request(req.body.scrapeUrl, async (err, resp, html) => {
-        if (!err && resp.statusCode == 200) {
-            try {
-                const $ = cheerio.load(html);
-                let json = [];
-                await $(req.body.section).each(async (i, data) => {
-                    const item1 = await sanitizeHtml($(data).html(), {
-                        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-                    }).replace(/(\r\n|\n|\r)/gm, "");
-
-                    if (req.body.restype && req.body.restype == "htmlString") {
-                        json += item1;
-                        return;
-                    }
-                    if (req.body.html) {
-                        json.push(item1);
-                        return;
-                    }
-                    var ajson = await himalaya.parse(item1);
-                    await json.push(ajson);
-                })
-                await setTimeout(async () => {
-                    await res.status(200).send({ data: json });
-                    // await res.status(200).send(json);
-                }, 100);
-                // console.log("here", json)
-            } catch (error) {
-                // console.log(error);
-                res.status(404).send({ err: "error" });
-
-            }
-        } else {
-            res.status(404).send({ err: "Can't Scrape" });
-        }
-    })
-            // res.status(404).send({ data: "fsdggfg" });
-});
 
 
 
